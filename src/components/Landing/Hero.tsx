2bb8638b28805e70f809c9c8f0378d30734b622f
okolo157 <victor.okolo@pau.edu.ng>
2026-04-13 06:26:28 +0100

    ttt

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Rocket } from "lucide-react";
 import { useNavigate } from "react-router-dom";
